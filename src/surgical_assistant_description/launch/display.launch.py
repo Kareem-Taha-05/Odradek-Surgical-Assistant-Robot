3c0950058f037245def1433d21d354d29908c80a
Kareem Taha <kmat2040@gmail.com>
2026-05-16 18:15:39 +0300

    updated display.launch.py

--- src/surgical_assistant_description/launch/display.launch.py
+++ src/surgical_assistant_description/launch/display.launch.py
@@ -4,7 +4,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue  # <-- We need this new import
+from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
     pkg_share = get_package_share_directory('surgical_assistant_description')
@@ -19,7 +19,7 @@ def generate_launch_description():
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': robot_description}] # <-- Updated parameter
+        parameters=[{'robot_description': robot_description}] 
     )
 
     joint_state_publisher_gui_node = Node(
